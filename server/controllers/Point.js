747d2435a14cad8c1c7784e50520343359d0e8a3
Reid Greer <jreid.greer@gmail.com>
2016-09-11 14:02:58 -0700

    Points update instead of duplicate

--- server/controllers/Point.js
+++ server/controllers/Point.js
@@ -1,6 +1,32 @@
 const db = require('../db');
 const logger = require('../utils/logger');
 
+exports.updatePoint = (data, cb) => {
+  const { user_id, brand_id, points } = data;
+  db.query('SELECT * FROM points WHERE user_id=$1 AND brand_id=$2', [user_id, brand_id], (err, result) => {
+    if (err) {
+      logger.error('error making new points', err);
+      cb(err, null);
+    } else if(!result.rows[0]){
+      logger.info('new points', data);
+      // Return null if unable to update
+      cb(null, null);
+    } else {
+      // Found entry, update
+      let totalPoints = points + result.rows[0].points;
+      db.query('UPDATE points SET points=$1 WHERE user_id=$2 AND brand_id=$3', [totalPoints, user_id, brand_id], (err, result) => {
+        if (err) {
+          logger.error('error updating points', err);
+          cb(err, null);
+        } else {
+          logger.info('new points', data);
+          cb(null, result.rows);
+        }
+      });
+    }
+  });
+}
+
 exports.createPoint = (data, cb) => {
   const { user_id, brand_id, points } = data;
   db.query('INSERT INTO points (user_id, brand_id, points) VALUES ($1, $2, $3)', [user_id, brand_id, points], (err, result) => {
